ba534207cc66fc740e7804f7fe95f3eaec5abf87
RizhabhArya <rishabharya9988@gmail.com>
2026-04-15 22:32:30 +0530

    Google Apps Script Web App

--- Remeowmber/api/sheets.js
+++ Remeowmber/api/sheets.js
@@ -1,14 +1,11 @@
 export default async function handler(req, res) {
-  const API_KEY = process.env.GOOGLE_API_KEY;
-  const SHEET_ID = "your_sheet_id";
-
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Sheet1?key=${API_KEY}`;
+  const url = "https://script.google.com/macros/s/AKfycbzZHaqWdIndsjHb5sFFe8uVnODEN1d-hYkJgSZjLqTcowN1hLDFzpPDGQ5puVPKbWaFzg/exec";
 
   try {
     const response = await fetch(url);
     const data = await response.json();
 
-    res.status(200).json(data); // <-- IMPORTANT
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
